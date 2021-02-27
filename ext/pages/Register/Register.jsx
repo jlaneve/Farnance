1d2d3b5bfe1b265a701061b321865a201933795e
Julian LaNeve <lanevejulian@gmail.com>
2021-02-27 11:01:04 -0600

    Create two user types using  toggle

--- ext/pages/Register/Register.jsx
+++ ext/pages/Register/Register.jsx
@@ -10,15 +10,17 @@ export default () => {
   const [email, setEmail] = useState()
   const [password, setPassword] = useState()
   const [submitError, setSubmitError] = useState()
+  const [userType, setUserType] = useState("farmer")
 
   // TODO: Do validation in form and show validation errors.
   // TODO: Turn into material-ui
 
   const handleSubmit = async (event) => {
+    console.log({ username, email, password, userType })
     event.preventDefault()
     setSubmitError(null)
     try {
-      await signup({ username, email, password })
+      await signup({ username, email, password, farmer: userType == "farmer" })
       await login(email, password)
       history.push('/')
     } catch (err) {
@@ -41,6 +43,13 @@ export default () => {
 
         <h2> Password </h2>
         <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+        <h2> User Type </h2>
+        <select value={userType} onChange={e => setUserType(e.target.value)}>
+            <option value="farmer">Farmer</option>
+            <option value="bank">Bank</option>
+        </select>
+
 
         <br />
         <input type='submit' value='Sign up' />
