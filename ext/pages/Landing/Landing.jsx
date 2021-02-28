20e2d6b967b9ec9f794ebb5b09f84c77cda6d6e2
Julian LaNeve <lanevejulian@gmail.com>
2021-02-28 10:26:36 -0600

    final commit

--- ext/pages/Landing/Landing.jsx
+++ ext/pages/Landing/Landing.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Typography, Grid, Container } from '@material-ui/core';
+import { Typography, Grid, Container, Button, Link, AppBar, Toolbar } from '@material-ui/core';
 
 import Header from '../Header/Header.jsx';
 
+import Favorite from '@material-ui/icons/Favorite';
+
+
 import logo from '../../images/logo.png'
 import bgImg from '../../images/farmers.jpg'
+import collateral from '../../images/collateral.png'
+import loan from '../../images/loan.png'
+
 
 import useAuth from '@wasp/auth/useAuth.js'
 
@@ -22,39 +28,65 @@ const Landing = () => {
             <Header />
             <div id="home" style={{background: "url(" + bgImg + ") no-repeat center fixed", height: "500px"}}>
                 <div className="landing-text">
-                    <h1>FARNANCE</h1>
-                    <h3>Funding Farmers and Smallholders through Digital Assets</h3>
+                    <Typography variant="h1">
+                        Farnance
+                    </Typography>
+                    <Typography variant="h5">
+                        Funding Farmers and Smallholders through Digital Assets
+                    </Typography>
                 </div>
             </div>
 
-        <Container maxWidth="md">
-            <Grid container spacing={3}>
+            <Container maxWidth="md">
+                <Grid container spacing={3} style={{paddingTop: "100px"}}>
+                    <Grid item xs={6}>
+                        <img src={logo} style={{ width: "50%", marginLeft: "25%" }} />
+                    </Grid>
 
-            </Grid>
-            {/* <div class="row my-10">
-                <div class="col-sm-6">
-                    <img src="images/logo.png" style="width:300px;height:300px" />
+                    <Grid item xs={6}>
+                        <Typography variant="h6" style={{top: "50%", transform: "translateY(-50%)", position: "relative"}}>
+                            Powered by blockchain, our platform aims to give farmers access to wider range of financing options.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Typography variant="h6" style={{top: "60%", transform: "translateY(-70%)", position: "relative"}}>
+                            Farmers can secure loans based on production instead of land.
+                        </Typography>
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <img src={collateral} style={{ width: "50%", marginLeft: "25%", marginTop: "25%" }} />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <img src={loan} style={{ width: "50%", marginLeft: "25%", marginTop: "25%" }} />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                        <Typography variant="h6" style={{top: "60%", transform: "translateY(-70%)", position: "relative"}}>
+                            Banks get direct access to farmers, can create loans on the open markets, and understand what their customers are willing to pay.
+                        </Typography>
+                    </Grid>
+                </Grid>
+
+                <Typography variant="h4" style={{paddingTop: "100px", textAlign: "center"}}>
+                    Ready to give it a try?
+                </Typography>
+
+                <div style={{marginTop: "30px", marginBottom: "30px", textAlign: "center"}}>
+                    <Button style={{fontSize: "125%"}} variant="contained" component={Link} href="/login">Login</Button>
+                    <Button style={{marginLeft: "40px", fontSize: "125%"}} variant="contained" color="primary" component={Link} href="/register">Register</Button>
                 </div>
-                <div class="col-sm-6 text-center">
-                    <p class="lead" style="padding-top: 100px;">Powered by blockchain, our platform aims to give farmers access to wider range of financing options.</p>
-                </div>
-            </div>
-            <div class="row my-10">
-                <div class="col-sm-6">
-                    <p class="lead" style="padding-top: 100px;">Farmers secure loans based on production instead of land.</p>
-                </div>
-                <div class="col-sm-6 text-center">
-                    <img src="images/loan.png" style="width:300px;height:300px">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6">
-                    <img src="images/banksfarms.png" style="width:300px;height:200px">
-                </div>
-                <div class="col-sm-6 text-center">
-                    <p class="lead" style="padding-top: 100px;">Banks get direct access to farmers.</p>
-                </div> */}
+
+                <br /><br /><br /><br /><br />
             </Container>
+
+            <AppBar position="static" style={{top: "auto", bottom: 0}}>
+                <Toolbar>
+                    <div style={{textAlign: "center", margin: "auto"}}>Made with <Favorite style={{fontSize: "14px"}} /> at <a href="hacklbs.org" style={{color: "white", textDecoration: "underline white dotted 3px"}}>HackLBS 2021</a>.</div>
+                </Toolbar>
+            </AppBar>
         </div>);
     }
 }
